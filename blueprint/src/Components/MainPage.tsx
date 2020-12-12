047442df48ee3b996404d3a81b30ef1de07427eb
jiyoung <qnfmtm666@naver.com>
2020-12-13 00:08:46 +0900

    2012130008 remove hellocontent & add searchbar response

--- blueprint/src/Components/MainPage.tsx
+++ blueprint/src/Components/MainPage.tsx
@@ -23,14 +23,20 @@ const MainPage: React.FC = (props) => {
   const setDocDataCallBack = (data: DocData) => {
     setDocData(data);
   };
+
   const helloRef = useRef<HTMLDivElement>(null);
+  const searchRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    const { current } = helloRef;
     if (docData != null) {
-      helloRef.current!.className = "opacity-0 transition duration-500";
-      setTimeout(() => {
-        helloRef.current!.className = "hidden";
-      }, 1500);
+      // helloRef.current!.className = "transition duration-500 ";
+      // setTimeout(() => {
+      //   helloRef.current!.className = "hidden";
+      //   searchRef.current!.className = "w-1/5";
+      // }, 1500);
+      // const rect = helloRef.current!.getBoundingClientRect();
+      // searchRef.current!.style.marginTop = -rect.top.toString() + "rem";
+      searchRef.current!.className =
+        "w-1/5 mt-16 transition transform duration-500 -translate-y-16";
     }
   }, [docData]);
   const api = {
@@ -39,13 +45,12 @@ const MainPage: React.FC = (props) => {
 
   return (
     <React.StrictMode>
-      <div className="flex flex-col h-100vh w-100vw justify-center items-center content-center -mt-32">
-        <div ref={helloRef}>
+      <div className="flex flex-col h-100vh w-100vw  items-center mt-8">
+        {/* <div ref={helloRef}>
           <HelloContent />
-        </div>
-
+        </div> */}
         <MainPageContext.Provider value={api}>
-          <div className="w-1/5">
+          <div className="w-1/5 mt-16" ref={searchRef}>
             <Search />
           </div>
         </MainPageContext.Provider>
@@ -58,7 +63,3 @@ const MainPage: React.FC = (props) => {
 };
 
 export default MainPage;
-
-const t = "this is the first Title";
-const c =
-  "언어에 따라 명사의 앞 뒤에 놓여지는 말이다. 이어지는 명사의 배경(수, 성 격 등)에 따라 변화 할 수도 있다. 지시사 등과 함께 한정하는 품사를 구성할 수도 있다 (영어 등). 한편, 지시사와 관사는 별도의 위치를 차지할 수 있다. 예를 들어, 카나 어에서 관사는 명사의 앞에, 지시사는 명사 뒤에 놓이고 동시에 사용할 수 있다.종종 접어이며, 또한 바로 뒤에 오는 단어의 발음에 따라 변화할 수있다. 예를 들어, 다음 단어의 어두가 모음일 때 다음이 자음일 때 비해 모음을 생략하고 자음을 보충하는 일이 자주 발생한다. 영어 관사는 다음이 자음일 때 생략된 발음을 한다.(an에서 n이 생략되어 발음)일부 언어에서는 전치사와 인접할 때 전치사와 결합하여 축약형이 될 수도 있다. 프랑스어에서는 축약형을 가진 조합 시에는 반드시 축약형을 사용한다. (예 : de + le → du ) 이러한 현상을 독일어에서는 의미의 차이로 구분한다. (예 : 보통 von + dem → vom 하지만 지시사인 경우 von dem처럼 한다) 이처럼 언어에 따라 형태가 달라진다. 또한, 로망스어군의 원류인 라틴어에는 관사가 없으며, 러시아어와 많은슬라브어, 그리고 페르시아어처럼 인도유럽어족에 속하는 언어도 관사가 없는 것도있다.";
